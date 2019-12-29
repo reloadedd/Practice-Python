fe0fbe2cbfbd9d099af16eb241031f634d9e9d19
reloadedd <reloadedd@protonmail.com>
2019-12-29 09:50:47 +0200

    Add solution for exercise 35

---
+++ 35_birthday_months.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+import json
+from collections import Counter
+
+filename = 'info.json'
+
+
+def identify_month_by_number(number: str) -> str:
+    if number == '01':
+        return 'January'
+    elif number == '02':
+        return 'February'
+    elif number == '03':
+        return 'March'
+    elif number == '04':
+        return 'April'
+    elif number == '05':
+        return 'May'
+    elif number == '06':
+        return 'June'
+    elif number == '07':
+        return 'July'
+    elif number == '08':
+        return 'August'
+    elif number == '09':
+        return 'September'
+    elif number == '10':
+        return 'October'
+    elif number == '11':
+        return 'November'
+    elif number == '12':
+        return 'December'
+
+
+if __name__ == '__main__':
+    with open(filename, 'r') as json_file:
+        birthdays = json.load(json_file)
+    scientist_months = [month.split('/')[0] for month in birthdays.values()]
+
+    counter = Counter(scientist_months)
+    print('{')
+    for month in counter:
+        print('\t{}: {},'.format(identify_month_by_number(month),
+                                 counter[month]))
+    print('}')
